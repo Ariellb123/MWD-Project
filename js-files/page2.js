0e51b271bea291aa201b7064686db5494d728f1f
Ariellb123 <82475217+Ariellb123@users.noreply.github.com>
2021-05-11 12:22:56 -0400

    Update page2.js

--- js-files/page2.js
+++ js-files/page2.js
@@ -1,20 +1,29 @@
 window.onload = () => {
 
-    let buttonOne = document.querySelector(`#button1`);
-    let inverted = false;
+    let videos = [];
+    videos[0] = document.querySelector(`#v1`);
+    videos[1] = document.querySelector(`#v2`);
+    videos[2] = document.querySelector(`#v3`);
+    videos[3] = document.querySelector(`#v4`);
+    videos[4] = document.querySelector(`#v5`);
+    videos[5] = document.querySelector(`#v6`);
 
-    let changeColor = () => {
-        if(!inverted) {
-            let background = document.querySelector(`.pageTwo`);
-            background.style.backgroundColor = `hsl(0, 0%, 25%)`;
-            inverted = true;
-        }
-        else {
-            let background = document.querySelector(`.pageTwo`);
-            background.style.backgroundColor = `hsl(0, 100%, 100%)`;
-            inverted = false;
-        }
-    }
-
-    buttonOne.addEventListener(`click`, changeColor);
+    setTimeout(function(){
+        videos[0].style.visibility = `visible`;
+        setTimeout(function(){
+            videos[1].style.visibility = `visible`;
+            setTimeout(function(){
+                videos[2].style.visibility = `visible`;
+                setTimeout(function(){
+                    videos[3].style.visibility = `visible`;
+                    setTimeout(function(){
+                        videos[4].style.visibility = `visible`;
+                        setTimeout(function() {
+                            videos[5].style.visibility = `visible`;
+                        }, 200);
+                    }, 200);
+                }, 200);
+            }, 200);
+        }, 200);
+    }, 200);
 }
